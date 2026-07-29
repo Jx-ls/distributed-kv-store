4258528d4597a386620ffee8f9a8b83e5296bf5a
Jx-ls <joshuarajtadi@gmail.com>
2026-07-30 04:05:09 +0530

    fix: get all keys

--- include/storage.h
+++ include/storage.h
@@ -2,12 +2,14 @@
 
 #include <string>
 #include <unordered_map>
+#include <vector>
 using namespace std;
 
 class Storage {
 public:
     void put(const string& key, const string& value);
     bool get(const string& key, string& value);
+    vector<string> get_all_keys() const;
 
 private:
     unordered_map<string, string> kv;
